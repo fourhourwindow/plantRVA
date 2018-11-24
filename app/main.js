64a2277f31a8d1615bb50e69c5db1e5e844d8633
sean cloney <seancloney@fourhourwindow.fios-router.home>
2018-11-24 12:20:26 -0500

    sticky header

--- app/main.js
+++ app/main.js
@@ -1,6 +1,40 @@
 $(document).ready(function(){
 
+  var winTop;
 
   //add year to copyright
   $('.year').text((new Date()).getFullYear());
+  //maxHeightForShrink = $('h1').offset().top - winTop ;
+  var headerHeight;
+//  var h1 = $('h1').position().top;
+
+  var diff = 0;
+
+  $(window).on('scroll resize',function(){
+    moveLogo();
+  });
+  function moveLogo(){
+
+    headerHeight = $('header').height();
+    winTop = $(window).scrollTop();
+
+    diff = headerHeight - winTop;
+      console.log('diff',diff)
+    if(diff < 50){
+      $('body').addClass('shrinkLogo');
+      // $('body .both').css({'transform': 'translate3d(0,'+(winTop * -1)+'px,0'});
+      // $('body header .bg').css({'transform': 'translate3d(0,'+winTop+'px,0'});
+
+    }
+
+    else{
+      $('body').removeClass('shrinkLogo');
+      $('body .both').css({'transform': 'translate3d(0,'+(winTop * -1)+'px,0'});
+      $('body header .bg').css({'transform': 'translate3d(0,'+winTop+'px,0'});
+
+      //$('body .both').css({'transform': 'translate3d(0,0,0'});
+      //$('body header .bg').css({'transform': 'translate3d(0,0,0'});
+    }
+  }
+    moveLogo();
 })
